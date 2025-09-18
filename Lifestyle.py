580692bd235c8ee72922f485a7385d12aac75423
Cerenshn0 <cerensahin095@gmail.com>
2025-09-19 01:07:49 +0300

    Update Lifestyle.py

--- Lifestyle.py
+++ Lifestyle.py
@@ -34,6 +34,66 @@ plt.ylabel("Sparing Time for Yourself")
 plt.show()
 
 
+#Satisfaction of Accomodation by Where You Stay
+plt.figure(figsize=(8,5))
+sns.countplot(x="Where_do_you_stay", 
+              hue="I_am_happy_with_my_accommodation",
+              data=df, 
+              order=df["Where_do_you_stay"].value_counts().index)
+plt.title("Satisfaction of Accomodation by Where You Stay")
+plt.xlabel("Where do you stay?")
+plt.ylabel("Count")
+plt.show()
+
+
+
+#Attendance Distribution by Stay Location
+plt.figure(figsize=(8,5))
+sns.boxplot(
+    x="Where_do_you_stay",
+    y="I_attend_classes",
+    data=df,
+    palette="Set2" )
+plt.title("Attendance Distribution by Stay Location")
+plt.xlabel("Where do you stay?")
+plt.ylabel("Attendance (1–5)")
+plt.show()
+
+print(df.loc[df["Where_do_you_stay"]=="At home with family","I_attend_classes"].value_counts())
+
+
+
+
+
+
+#
+grades = df["What_grade_are_you_in"].unique()
+fig, axes = plt.subplots(2, 3, figsize=(15,10), sharex=True)
+
+for ax, grade in zip(axes.flat, grades):
+    sns.barplot(
+        data=df[df["What_grade_are_you_in"]==grade],
+        x="What_is_your_CGPA",
+        y="How_much_time_do_you_spend_for_your_social_life_per_day",
+        estimator=lambda x: x.mean(),
+        palette="coolwarm",
+        orient="h",
+        ax=ax
+    )
+    ax.set_title(f"Grade {grade}")
+    ax.set_xlabel("Average CGPA")                     # x label
+    ax.set_ylabel("Social Life per Day") 
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
 # Social Life Distribution
 plt.figure(figsize=(8,5))
 sns.barplot(
